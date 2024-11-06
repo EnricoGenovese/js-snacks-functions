d4d5ce5394e08bb34caccf72d73319e4d7be0d2c
EnricoGenovese <enricogeno@libero.it>
2024-11-06 17:58:57 +0100

    exercise 3 arrow function;

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -6,24 +6,37 @@ const word = 'javascript';
 // Dichiara la funzione qui.
 
 
-function printVowels(string) {
-    let vowels = ["a", "e", "i", "o", "u"];         // i caratteri che, se trovati, fanno salire il contatore
-    let vowelsNumb = 0;                             // varaibile di accumulo
+function printVowels(string) {  // i caratteri che, se trovati, fanno salire il contatore
+    let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];         
+    let vowelsNumb = 0;
+    let vowelsFound = []                             // varaibile di accumulo
     //
-    for(let i = 0; i <= string.length; i++) {       // ciclo per per leggere tutti i caratteri nella stringa
+    for(let i = 0; i < string.length; i++) {       // ciclo per per leggere tutti i caratteri nella stringa
         //
-        if(vowels.indexOf(string[i]) !== -1) {      // se si trova una corrispondenza tra un elemento di  
-                                                    // 'vowels' e un l'index ciclato della stringa
+        if(vowels.includes(string[i])) {            // se si trova una corrispondenza tra un elemento di  
+            vowelsFound.push(string[i]);                                       // 'vowels' e un l'index ciclato della stringa
             vowelsNumb += 1;                        // il contatore sale di uno
         }
     }
-    return vowelsNumb;
+    return vowelsNumb
 }
 
-
+const countArrow = (string) => {
+    let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+    let vowelsNumb = 0;
+    let vowelsFound = [] 
+    for(let i = 0; i < string.length; i++) {
+        if(vowels.includes(string[i])) {
+            vowelsFound.push(string[i]);
+            vowelsNumb += 1;  
+        }              
+    }
+    return vowelsNumb; 
+}
 // Invoca la funzione qui e stampa il risultato in console
 
 const vowelsJS = printVowels(word);
+console.log(countArrow(word));
 console.log(vowelsJS);
 
 
