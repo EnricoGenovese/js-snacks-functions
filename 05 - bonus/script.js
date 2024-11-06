e9c115c3b78bb4856415c261c8a24a153f1e3a11
EnricoGenovese <enricogeno@libero.it>
2024-11-06 18:20:17 +0100

    exercise 5;

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -10,6 +10,20 @@ const name = 'Mario';
 
 // Dichiara la funzione qui.
 
+function timeGreeting(string) {
+    let date = new Date();
+    if(date.getHours() >= 6 && date.getHours() < 13) {
+        console.log("Buongiono " + string);
+    } else if (date.getHours() >= 13 && date.getHours() < 17) {
+        console.log("Buon pomeriggio " + string)
+    } else if(date.getHours() > 17 && date.getHours() < 23) {
+        console.log("Buonasera " + string);
+    } else if (date.getHours() < 6) {
+        console.log("Buonanotte" + string);
+    }
+}
+
+timeGreeting(name);
 
 // Invoca la funzione qui e stampa il risultato in console
 
