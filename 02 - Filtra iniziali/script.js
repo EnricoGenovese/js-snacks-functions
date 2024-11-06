f6b5cf6c63454bf13d1dd4f4225d57b187b08e3d
EnricoGenovese <enricogeno@libero.it>
2024-11-06 14:55:33 +0100

    exercise 2;

--- 02 - Filtra iniziali/script.js
+++ 02 - Filtra iniziali/script.js
@@ -5,9 +5,20 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Laura", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function filterInitial(haystack, needle) {
+    let newArray = [];
+    for( let i = 0; i < haystack.length; i++) {
+        if(haystack[i].charAt(0) === needle) {
+            newArray.push(haystack[i]);
+        }
+    }
+    return newArray; 
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+const onlyA = filterInitial(names, "A");
+console.log(onlyA);
 
 
 //Risultato atteso se si passa la lettera A: ["Anna", "Adele", "Alessandra"]
