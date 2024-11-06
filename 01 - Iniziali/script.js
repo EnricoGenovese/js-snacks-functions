7478a58ccb0306c57131936a4e9f23850f824efb
EnricoGenovese <enricogeno@libero.it>
2024-11-06 14:49:38 +0100

    exercise 1;

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -5,9 +5,17 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function nameInitial(array) {
+    let charZero = [];
+    for(let i = 0; i < array.length; i++) {
+        charZero.push(array[i].charAt(0));
+    }
+    return charZero;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const fistLetter = nameInitial(names);
+console.log(fistLetter);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
